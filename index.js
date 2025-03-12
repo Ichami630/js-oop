fc2a41c0999460254c91e5557b638c511cb1a92d
ichami630 <brandonichami@gmail.com>
2025-03-12 12:24:49 +0100

    prototypes and inheritance

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ function Person(name, age){
     this.greet = function (){
         console.log(`Hey ${this.name} you are ${this.age} years old`)
     }
+    //prototype and inheritance
+    //methods defined on the prototypes are shared on all instances
+    Person.prototype.sayHi = () =>{
+        console.log(`Hi ${this.name}`)    
+    }
+    //saves memory because methods aren't duplicated for every object
 }
 
 const p1 = new Person("ichami",10)
 p1.greet()
+p1.sayHi()
